c0eec01c5adbb4de77f7720281663615a78a96da
lebritish <tcmartin@bluewin.ch>
2017-06-25 01:41:45 +0200

    adds comments to average and normalised vector models

--- average_vector.py
+++ average_vector.py
@@ -4,32 +4,55 @@ import time as timer
 import data_utils as du
 
 def train(trainX, trainY, Z):
+	"""
+	@param trainX : a matrix containing a bag of words representation of a text ie for a row each column represents the number of occurrences of a certain word
+	@param trainY : a matrix of five columns for which every row has one column at 1 and the others at 0 indicating what the score is
+	@param Z : a matrix for which every row is a vector representing a word
+
+	returns : the average vector for every score
+	"""
+	# we start by declaring the necessary variables
 	scoreVectors = np.zeros((5,Z.shape[1]))
 	vectorCounts = np.zeros(5)
 
+	# we then go through the instances
 	for i in range(trainX.shape[0]):
+		# we get the instance's score
 		score = du.get_score(i, trainY)
+		# we then go through the instance to get the scores' sum of vectors
 		for j in range(trainX.shape[1]):
 			if trainX[i, j] != 0:
 				scoreVectors[score-1, :] += trainX[i, j]*Z[j, :]
 				vectorCounts[score-1] += trainX[i, j]
 	
+	# we get the average vector of a score
 	for i in range(5):
 		scoreVectors[i, :] /= vectorCounts[i]
 
 	return scoreVectors
 
 def guess_score(xRow, Z, scoreVectors):
+	"""
+	@param xRow : a bag of words representation of an instance
+	@param Z : a matrix for which every row is a vector representing a word
+	@param scoreVectors : a matrix for which every row is the average vector for a score
+
+	returns : an estimate of the score based on the nearest average score vector to the average instance vector
+	"""
+	# we declare necessary variables
 	averageVector = np.zeros(Z.shape[1])
 	vectorCount = 0
 	
+	# we sum up the vectors of an instance
 	for i in range(len(xRow)):
 		if xRow[i] != 0:
 			averageVector += xRow[i]*Z[i, :]
 			vectorCount += xRow[i]
 
+	# we get the average vector for the instance
 	averageVector /= vectorCount
 
+	# we find the nearest score average vector to the instance average vector
 	score = 1
 	minScoreDist = np.linalg.norm(averageVector-scoreVectors[0, :])
 	
@@ -41,33 +64,54 @@ def guess_score(xRow, Z, scoreVectors):
 	return score
 
 def get_performances(trainX, testX, trainY, testY, Z):
+	"""
+	@param trainX : a matrix containing the training instances in bag of words format
+	@param testX : a matrix containing the test instances in bag of words format, the instances are different from those of trainX
+	@param trainY : a matrix of five columns for which every row has one column at 1 and the others at 0 indicating what the score is
+	@param testY : a matrix similar to trainY containing different instances	@param Z : a matrix containing a vector representation of the words in a dictionnary
+
+	returns : the model's raw success rate, its confusion matrix and the training and testing times
+	"""
+	# we start the training timer
 	trainingStart = timer.time()
 	
+	# we get the average score vector
 	scoreVectors = train(trainX, trainY, Z)
 
+	# we stop the training timer
 	trainingEnd = timer.time()
 	
+	# we get the training time
 	trainingTime = trainingEnd-trainingStart
 
+	# we start the training time
 	testingStart = timer.time()
 
+	# we declare necessary variables for evaluation
 	successRate = 0
 	confusionMatrix = np.zeros((5,5))
 
+	# we go through every instance
 	for i in range(testX.shape[0]):
+		# we get the actual score and the estimated score
 		actualScore = du.get_score(i, testY)
 		guessedScore = guess_score(testX[i, :], Z, scoreVectors)
 
+		# if the score is correct we increment the success rate
 		if actualScore == guessedScore:
 			successRate += 1
 
+		# we increment the adequate element of the confusion matrix
 		confusionMatrix[actualScore-1, guessedScore-1] += 1
 
+	# we convert the measures to percentiles
 	successRate /= testX.shape[0]
 	confusionMatrix /= testX.shape[0]
 
+	# we stop the testing timer
 	testingEnd = timer.time()
 
+	# we get the testing time
 	testingTime = testingEnd-testingStart
 
 	return [successRate, confusionMatrix, trainingTime, testingTime]
--- normalised_vector.py
+++ normalised_vector.py
@@ -4,28 +4,51 @@ import time as timer
 import data_utils as du
 
 def train(trainX, trainY, Z):
+	"""
+	@param trainX : a matrix containing a bag of words representation of a text ie for a row each column  represents the number of occurrences of a certain word
+	@param trainY : a matrix of five columns five columns for which every row has one column at 1 and the others at 0 indicating what the score is
+	@param Z : a matrix : a matrix which every row is a vector representing a word
+
+	returns the normalised sum of vectors for every score
+	"""
+	# we declare the necessary variable
 	scoreVectors = np.zeros((5,Z.shape[1]))
 
+	# we go through the instances
 	for i in range(trainX.shape[0]):
+		# we get the instanceŝ score
 		score = du.get_score(i, trainY)
 		for j in range(trainX.shape[1]):
+			# we sum up the present word vectors
 			if trainX[i, j] != 0:
 				scoreVectors[score-1, :] += trainX[i, j]*Z[j, :]
 	
+	# we normalise the vectors
 	for i in range(5):
 		scoreVectors[i, :] /= np.linalg.norm(scoreVectors[i, :])
 
 	return scoreVectors
 
 def guess_score(xRow, Z, scoreVectors):
+	"""
+	@param xRow : a bag of words representation of an instance
+	@param Z : a matrix for which every row is a vector representing a word
+	@param scoreVectors : a matrix for which every row is the normalised sum of vectors present in a score
+
+	returns : an estimate of the score based on the nearest normalised score vector
+	"""
+	# we declare the necessary variable
 	averageVector = np.zeros(Z.shape[1])
 	
+	# we sum up the vectors of the instance
 	for i in range(len(xRow)):
 		if xRow[i] != 0:
 			averageVector += xRow[i]*Z[i, :]
 
+	# we normalise the instance
 	averageVector /= np.linalg.norm(averageVector)
 
+	# we find the nearest normalised score vector to the normalised instance vector
 	score = 1
 	minScoreDist = np.linalg.norm(averageVector-scoreVectors[0, :])
 	
@@ -37,33 +60,55 @@ def guess_score(xRow, Z, scoreVectors):
 	return score
 
 def get_performances(trainX, testX, trainY, testY, Z):
+	"""
+	@param trainX : a matrix containing the training instances in bag of words format
+	@param testX : a matrix the test instances in bag of words format, the instances are different from those in trainX
+	@param trainY : a matrix of five columns for which every row has one column at 1 and the others at 0 indicating what the score is
+	@param testY _ a matrix similar to trainY containing different instances
+	@param Z : a matrix containing a vector representation of the words in adictionnary
+
+	returns : the model's raw success rate, its confusion matrix and the training and testing times
+	"""
+	# we start the training timer
 	trainingStart = timer.time()
 	
+	# we get the normalised score vectors
 	scoreVectors = train(trainX, trainY, Z)
 
+	# we stop the training timer
 	trainingEnd = timer.time()
 	
+	# we get the training time
 	trainingTime = trainingEnd-trainingStart
 
+	# we start the testing timer
 	testingStart = timer.time()
 
+	# we declare the necessary variables for performace evaluation
 	successRate = 0
 	confusionMatrix = np.zeros((5,5))
 
+	# we go through the test instances
 	for i in range(testX.shape[0]):
+		# we get the actual score and the estimated score
 		actualScore = du.get_score(i, testY)
 		guessedScore = guess_score(testX[i, :], Z, scoreVectors)
 
+		# if the guessed score is correct we increment the the success rate
 		if actualScore == guessedScore:
 			successRate += 1
 
+		# we increment the adequate element of the confusion matrix
 		confusionMatrix[actualScore-1, guessedScore-1] += 1
 
+	# we convert the variables to percentiles
 	successRate /= testX.shape[0]
 	confusionMatrix /= testX.shape[0]
 
+	# we stop the testing timer
 	testingEnd = timer.time()
 
+	# we get the testing time
 	testingTime = testingEnd-testingStart
 
 	return [successRate, confusionMatrix, trainingTime, testingTime]
